97e5bea91115a4c9669167c3887703a4b64f8f3c
Hiroshi Miura <miurahr@linux.com>
2021-10-15 08:02:59 +0900

    test: encode and decode
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_bcj_misc.py
+++ tests/test_bcj_misc.py
@@ -6,26 +6,32 @@ import bcj
 
 
 def test_aarch64_encode(tmp_path):
-    with open(pathlib.Path(__file__).parent.joinpath('data/lib/aarch64-linux-gnu/liblzma.so.0'), 'rb') as f:
+    with open(
+        pathlib.Path(__file__).parent.joinpath("data/lib/aarch64-linux-gnu/liblzma.so.0"),
+        "rb",
+    ) as f:
         src = f.read()
     encoder = bcj.ARMEncoder()
     dest = encoder.encode(src)
     dest += encoder.flush()
-    with open(tmp_path.joinpath('output.bin'), 'wb') as f:
+    with open(tmp_path.joinpath("output.bin"), "wb") as f:
         f.write(dest)
     m = hashlib.sha256()
     m.update(dest)
-    assert m.digest() == binascii.unhexlify('be4b1217015838b417a255a3bb1d17ec8a9357c0e195b00bcd5b48959aac8295')
+    assert m.digest() == binascii.unhexlify("be4b1217015838b417a255a3bb1d17ec8a9357c0e195b00bcd5b48959aac8295")
 
 
 def test_ppc_encode(tmp_path):
-    with open(pathlib.Path(__file__).parent.joinpath('data/lib/powerpc64le-linux-gnu/liblzma.so.0'), 'rb') as f:
+    with open(
+        pathlib.Path(__file__).parent.joinpath("data/lib/powerpc64le-linux-gnu/liblzma.so.0"),
+        "rb",
+    ) as f:
         src = f.read()
     encoder = bcj.PPCEncoder()
     dest = encoder.encode(src)
     dest += encoder.flush()
-    with open(tmp_path.joinpath('output.bin'), 'wb') as f:
+    with open(tmp_path.joinpath("output.bin"), "wb") as f:
         f.write(dest)
     m = hashlib.sha256()
     m.update(dest)
-    assert m.digest() == binascii.unhexlify('0289683dfa366682f0d6cc17880ed64b1f98ab889bc62dff0b9098bcdd8d4af3')
+    assert m.digest() == binascii.unhexlify("0289683dfa366682f0d6cc17880ed64b1f98ab889bc62dff0b9098bcdd8d4af3")
--- tests/test_bcj_x86.py
+++ tests/test_bcj_x86.py
@@ -4,27 +4,76 @@ import pathlib
 
 import bcj
 
+BLOCKSIZE = 8192
+
 
 def test_x86_encode(tmp_path):
-    with open(pathlib.Path(__file__).parent.joinpath('data/x86.bin'), 'rb') as f:
+    with open(pathlib.Path(__file__).parent.joinpath("data/x86.bin"), "rb") as f:
         src = f.read()
     encoder = bcj.BCJEncoder()
     dest = encoder.encode(src)
     dest += encoder.flush()
-    with open(tmp_path.joinpath('output.bin'), 'wb') as f:
+    with open(tmp_path.joinpath("output.bin"), "wb") as f:
         f.write(dest)
     m = hashlib.sha256()
     m.update(dest)
-    assert m.digest() == binascii.unhexlify('e396dadbbe0be4190cdea986e0ec949b049ded2b38df19268a78d32b90b72d42')
+    assert m.digest() == binascii.unhexlify("e396dadbbe0be4190cdea986e0ec949b049ded2b38df19268a78d32b90b72d42")
 
 
 def test_x86_decode(tmp_path):
-    with open(pathlib.Path(__file__).parent.joinpath('data/bcj.bin'), 'rb') as f:
+    with open(pathlib.Path(__file__).parent.joinpath("data/bcj.bin"), "rb") as f:
         src = f.read()
     decoder = bcj.BCJDecoder(len(src))
     dest = decoder.decode(src)
-    with open(tmp_path.joinpath('output.bin'), 'wb') as f:
+    with open(tmp_path.joinpath("output.bin"), "wb") as f:
         f.write(dest)
     m = hashlib.sha256()
     m.update(dest)
-    assert m.digest() == binascii.unhexlify('5ae0726746e2ccdad8f511ecfcf5f79df4533b83f86b1877cebc07f14a4e9b6a')
+    assert m.digest() == binascii.unhexlify("5ae0726746e2ccdad8f511ecfcf5f79df4533b83f86b1877cebc07f14a4e9b6a")
+
+
+def test_x86_encode_decode1(tmp_path):
+    with open(pathlib.Path(__file__).parent.joinpath("data/x86.bin"), "rb") as f:
+        src = f.read()
+    encoder = bcj.BCJEncoder()
+    dest = encoder.encode(src)
+    dest += encoder.flush()
+    with open(tmp_path.joinpath("output.bin"), "wb") as f:
+        f.write(dest)
+    m = hashlib.sha256()
+    m.update(dest)
+    assert m.digest() == binascii.unhexlify("e396dadbbe0be4190cdea986e0ec949b049ded2b38df19268a78d32b90b72d42")
+    with open(tmp_path.joinpath("output.bin"), "rb") as f:
+        src = f.read()
+    decoder = bcj.BCJDecoder(len(src))
+    dest = decoder.decode(src)
+    m = hashlib.sha256()
+    m.update(dest)
+    assert m.digest() == binascii.unhexlify("5ae0726746e2ccdad8f511ecfcf5f79df4533b83f86b1877cebc07f14a4e9b6a")
+
+
+def test_x86_encode_decode2(tmp_path):
+    dest = bytearray()
+    encoder = bcj.BCJEncoder()
+    with open(pathlib.Path(__file__).parent.joinpath("data/x86.bin"), "rb") as f:
+        src = f.read(BLOCKSIZE)
+        while len(src) > 0:
+            dest += encoder.encode(src)
+            src = f.read(BLOCKSIZE)
+    dest += encoder.flush()
+    with open(tmp_path.joinpath("output.bin"), "wb") as f:
+        f.write(dest)
+    size = len(dest)
+    m = hashlib.sha256()
+    m.update(dest)
+    assert m.digest() == binascii.unhexlify("e396dadbbe0be4190cdea986e0ec949b049ded2b38df19268a78d32b90b72d42")
+    #
+    dest = bytearray()
+    decoder = bcj.BCJDecoder(size)
+    with open(tmp_path.joinpath("output.bin"), "rb") as f:
+        src = f.read(BLOCKSIZE)
+        while len(src) > 0:
+            dest += decoder.decode(src)
+            src = f.read(BLOCKSIZE)
+    m = hashlib.sha256(dest)
+    assert m.digest() == binascii.unhexlify("5ae0726746e2ccdad8f511ecfcf5f79df4533b83f86b1877cebc07f14a4e9b6a")
