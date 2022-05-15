4b78f6c406d134efbf5268cc0cc55c3d1f3eec6f
Hiroshi Miura <miurahr@linux.com>
2022-05-15 12:28:08 +0900

    add complex test case for bcj filter
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_bcj_x86.py
+++ tests/test_bcj_x86.py
@@ -77,3 +77,44 @@ def test_x86_encode_decode2(tmp_path):
             src = f.read(BLOCKSIZE)
     m = hashlib.sha256(dest)
     assert m.digest() == binascii.unhexlify("5ae0726746e2ccdad8f511ecfcf5f79df4533b83f86b1877cebc07f14a4e9b6a")
+
+
+def test_x86_encode_decode_complex(tmp_path):
+    hashsrc = list()
+    hashresult = list()
+    exe_path = pathlib.Path(__file__).parent.joinpath("data/x86")
+    dest = bytearray()
+    encoder = bcj.BCJEncoder()
+    for i in range(8):
+        m = hashlib.sha256()
+        refpath = exe_path.joinpath(f"{i}.exe")
+        with open(refpath, "rb") as f:
+            src = f.read(BLOCKSIZE)
+            while len(src) > 0:
+                m.update(src)
+                dest += encoder.encode(src)
+                src = f.read(BLOCKSIZE)
+        hashsrc.append(m.digest())
+    dest += encoder.flush()
+    total_length = len(dest)
+    #
+    with open(tmp_path.joinpath("output.bin"), "wb") as f:
+        f.write(dest)
+    #
+    decoder = bcj.BCJDecoder(total_length)
+    with open(tmp_path.joinpath("output.bin"), "rb") as f:
+        for i in range(8):
+            refpath = exe_path.joinpath(f"{i}.exe")
+            dest = bytearray()
+            m = hashlib.sha256()
+            remaining = refpath.stat().st_size
+            while remaining > 0:
+                size = min(remaining, BLOCKSIZE)
+                src = f.read(size)
+                tmp = decoder.decode(src)
+                remaining -= len(tmp)
+                dest += tmp
+            m.update(dest)
+            hashresult.append(m.digest())
+    for i in range(8):
+        assert hashresult[i] == hashsrc[i], f"filter error in {i}.exe"
